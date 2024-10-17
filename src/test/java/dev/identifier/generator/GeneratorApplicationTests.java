e53a786cbae5fc59644a46f6b9f7374171b0ede8
Argenis Rodriguez <argenismrvel@gmail.com>
2024-10-16 23:41:44 -0400

    Se agregan archivos faltantes

---
+++ src/test/java/dev/identifier/generator/GeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.identifier.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
